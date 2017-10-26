ee92a922e3a302b64eff6a6b1733aef3a3b979bb
Drew <sombersunday@gmail.com>
2017-10-26 10:31:34 -0400

    Add project files.

---
+++ ConsoleAppExerciseInheritance/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppExerciseInheritance
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Test Implementation Code for Exercise
+            var stack = new Stack();
+            
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            //Intended output should be: 3, 2, 1
+
+
+            //Personal Unit Tests
+            var stack2 = new Stack();
+
+            //Console.WriteLine(stack2.Pop()); //Test Empty Stack Pop
+            //stack2.Push(null); //Test Null stack Push
+            stack2.Push(1);
+            stack2.Push(2);
+            stack2.Push(3);
+
+            Console.WriteLine(stack2.Pop());
+            Console.WriteLine(stack2.Pop());
+            Console.WriteLine(stack2.Pop());
+            
+            //Clean up our Stack
+            stack2.Clear();
+
+        }
+    }
+}
+
---
+++ ConsoleAppExerciseInheritance/Stack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleAppExerciseInheritance
+{
+    class Stack
+    {
+        ArrayList stackList = new ArrayList();
+
+        public void Push(Object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("Push cannot be null");
+            else
+                stackList.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (stackList.Count == 0)
+                throw new InvalidOperationException("Stack is Empty");
+            else
+            {
+                //Return the OBJECT at the end of the Stack
+                //First IN, LAST OUT
+                int lastElement = stackList.Count - 1;
+                object returnValue = stackList[lastElement];
+                stackList.RemoveAt(lastElement);
+
+                return returnValue;
+            }
+        }
+
+        public void Clear()
+        {
+            stackList.Clear();
+            Console.WriteLine("Stack Cleared");
+        }
+    }
+}
+
