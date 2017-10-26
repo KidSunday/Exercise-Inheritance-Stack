d1a8c0fcaeacca908b7186f8774f68981945cfe6
Drew <sombersunday@gmail.com>
2017-10-26 11:47:06 -0400

    Set Private level to ArrayList

--- ConsoleAppExerciseInheritance/Stack.cs
+++ ConsoleAppExerciseInheritance/Stack.cs
@@ -5,27 +5,33 @@ namespace ConsoleAppExerciseInheritance
 {
     class Stack
     {
-        ArrayList stackList = new ArrayList();
+        private ArrayList _stackList;
+
+        public Stack()
+        {
+            _stackList = new ArrayList();
+        }
+        
 
         public void Push(Object obj)
         {
             if (obj == null)
                 throw new InvalidOperationException("Push cannot be null");
             else
-                stackList.Add(obj);
+                _stackList.Add(obj);
         }
 
         public object Pop()
         {
-            if (stackList.Count == 0)
+            if (_stackList.Count == 0)
                 throw new InvalidOperationException("Stack is Empty");
             else
             {
                 //Return the OBJECT at the end of the Stack
                 //First IN, LAST OUT
-                int lastElement = stackList.Count - 1;
-                object returnValue = stackList[lastElement];
-                stackList.RemoveAt(lastElement);
+                int lastElement = _stackList.Count - 1;
+                object returnValue = _stackList[lastElement];
+                _stackList.RemoveAt(lastElement);
 
                 return returnValue;
             }
@@ -33,7 +39,7 @@ namespace ConsoleAppExerciseInheritance
 
         public void Clear()
         {
-            stackList.Clear();
+            _stackList.Clear();
             //Console.WriteLine("Stack Cleared"); 
         }
     }
